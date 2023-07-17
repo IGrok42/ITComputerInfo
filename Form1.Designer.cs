11955e0310ef025c01d502907f96a3d25288a90a
Jim McGinnis <jamesm2980@gmail.com>
2023-07-17 09:36:14 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ITComputerInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblMain = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(476, 363);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(130, 41);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblMain
+            // 
+            this.lblMain.AutoSize = true;
+            this.lblMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMain.Location = new System.Drawing.Point(498, 145);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(42, 15);
+            this.lblMain.TabIndex = 1;
+            this.lblMain.Text = "lblMain";
+            this.lblMain.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-1, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(348, 153);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblMain);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblMain;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;   //This namespace is used to work with WMI classes. For using this namespace add reference of System.Management.dll .
+using Microsoft.Win32;     //This namespace is used to work with Registry editor.
+
+namespace ITComputerInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            lblMain.Text = "Hello World!";
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Displaying operating system info....\n");
+            //Create an object of ManagementObjectSearcher class and pass query as parameter.
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+            foreach (ManagementObject managementObject in mos.Get())
+            {
+                if (managementObject["Caption"] != null)
+                {
+                    textBox1.Text = "Operating System Name  :  " + managementObject["Caption"].ToString();   //Display operating system caption
+                }
+                //if (managementObject["OSArchitecture"] != null)
+                //{
+                //    Console.WriteLine("Operating System Architecture  :  " + managementObject["OSArchitecture"].ToString());   //Display operating system architecture.
+                //}
+                //if (managementObject["CSDVersion"] != null)
+                //{
+                //    Console.WriteLine("Operating System Service Pack   :  " + managementObject["CSDVersion"].ToString());     //Display operating system version.
+                //}
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+            foreach (ManagementObject managementObject in mos.Get())
+            {
+                if (managementObject["Caption"] != null)
+                {
+                    textBox1.Text = "Operating System Name  :  " + managementObject["Caption"].ToString();   //Display operating system caption
+                }
+                if (managementObject["OSArchitecture"] != null)
+                {
+                    textBox1.Text += "\r\nOperating System Architecture  :  " + managementObject["OSArchitecture"].ToString();   //Display operating system architecture.
+                }
+                if (managementObject["CSDVersion"] != null)
+                {
+                    textBox1.Text += "\r\nOperating System Service Pack   :  " + managementObject["CSDVersion"].ToString();     //Display operating system version.
+                }
+                if (managementObject["CSName"] != null)
+                {
+                    textBox1.Text += "\r\nCSName   :  " + managementObject["CSName"].ToString();     //Display operating system version.
+                }
+            }
+            ManagementObjectSearcher mos2 = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
+            foreach (ManagementObject managementObject in mos2.Get())
+            {
+                if (managementObject["Manufacturer"] != null)
+                {
+                    textBox1.Text += "\r\nManufacturer :  " + managementObject["Manufacturer"].ToString();   //Display operating system caption
+                }
+                if (managementObject["Model"] != null)
+                {
+                    textBox1.Text += "\r\nModel  :  " + managementObject["Model"].ToString();   //Display operating system architecture.
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
